38c037d8d3261378fb050167abf18e9904564235
Mauro Colella <mauro.ludovico.colella@gmail.com>
2022-08-06 19:20:55 +0800

    Format scene

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ type SpherePropType = ThreeElements['mesh'] & {
   radius?: number,
   widthSegments?: number,
   heightSegments?: number,
+  wireframe?: boolean,
+  color?: string,
 }
 
 function Sphere(props: SpherePropType) {
   const ref = useRef<THREE.Mesh>(null!)
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  useFrame((state, delta) => (ref.current.rotation.y -= 0.001))
   return (
     <mesh
       {...props}
@@ -23,21 +25,38 @@ function Sphere(props: SpherePropType) {
           props.heightSegments ?? 36,
         ]}
       />
-      <meshStandardMaterial color={'blue'} />
+      <meshStandardMaterial
+        wireframe={props.wireframe ?? false}
+        color={props.color ?? 'white'}
+      />
     </mesh>
   )
 }
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Canvas>
+    <Canvas
+      orthographic
+      camera={{
+        zoom: 720,
+      }}
+    >
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight position={[10, 10, 1000]} />
       <Sphere
-        position={[0, 0, 0]}
-        radius={2}
+        position={[-0.5, 0, 0]}
+        radius={1}
         widthSegments={72}
         heightSegments={72}
+        color={'white'}
+      />
+      <Sphere
+        position={[-0.5, 0, 0]}
+        radius={1.004}
+        widthSegments={36}
+        heightSegments={36}
+        color={'#cccccc'}
+        wireframe
       />
     </Canvas>
   </React.StrictMode>,
